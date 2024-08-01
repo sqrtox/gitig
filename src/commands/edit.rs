4de76a6c82c803e2d91ebec46fd8b0636b0461e4
sqrtox <84203131+sqrtox@users.noreply.github.com>
2024-08-01 15:43:37 +0900

    edit command with syntax highlight

--- src/commands/edit.rs
+++ src/commands/edit.rs
@@ -1,14 +1,14 @@
 use crate::db::{resolve_profile_name, Database, Profile};
 use crate::error::Result;
 use crate::message;
-use edit::edit as edit_contents;
+use edit::{edit_with_builder, Builder};
 
 pub fn edit(profile_name: Option<String>) -> Result<()> {
     let profile_name = resolve_profile_name(profile_name)?;
     let db = Database::connect()?;
 
     if let Some(profile) = db.select_profile_by_name(&profile_name)? {
-        let body = edit_contents(profile.body)?;
+        let body = edit_with_builder(profile.body, Builder::new().suffix(".gitconfig"))?;
 
         db.update_profile_body(&Profile {
             name: profile.name,
@@ -21,7 +21,7 @@ pub fn edit(profile_name: Option<String>) -> Result<()> {
             message::identifier(&profile_name)
         );
     } else {
-        let body = edit_contents("")?;
+        let body = edit_with_builder("", Builder::new().suffix(".gitconfig"))?;
 
         db.insert_profile(&Profile {
             name: profile_name.clone(),
